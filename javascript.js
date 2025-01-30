4bda358413fae510682988bb59d3269e7e03e7fa
BottomToTheTop <121840768+BottomToTheTop@users.noreply.github.com>
2025-01-30 03:06:20 +0000

    Add computer choice logic

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let computerChoice = Math.random();
+    console.log(computerChoice);
+    if (computerChoice < 0.33) {
+        return "rock";
+    } else if (computerChoice < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
